eb79d4ee8e1827faee769f53bfa0ec830a3000e1
Ken Newman <WraithKenny>
2012-06-16 17:06:06 -0400

    code-editor cleanups

--- js/code-editor.js
+++ js/code-editor.js
@@ -4,7 +4,7 @@ jQuery( document ).ready( function( $ ) {
 	var theme = codemirror_options.theme ? codemirror_options.theme: 'default';
 	var file = $( 'input[name="file"]' ).val();
 	var fileType = file.slice( file.lastIndexOf(".")+1 );
-	
+	var $new = $( '#newcontent' );
 	var $submitWrap = $( '#submit' ).parent();
 	var modHeight = $submitWrap.height()
 		+ parseInt( $submitWrap.css( 'marginTop' ) ) 
@@ -17,28 +17,38 @@ jQuery( document ).ready( function( $ ) {
 		modHeight += $documentation.height() + parseInt( $documentation.css( 'marginTop' ) );
 	}
 	
-	var height = $(window).height() - $( '#newcontent' ).offset().top - $( '#wpadminbar' ).height() - modHeight;
+	var height = $(window).height() - $new.offset().top - $( '#wpadminbar' ).height() - modHeight;
 	
 	var config = { lineNumbers: true, mode: "javascript", theme: theme };
 	
-	if ( "js" == fileType )
-		config.mode = "javascript";
-	else if ( "css" == fileType )
-		config.mode = "css";
-	else if ( "less" == fileType )
-		config.mode = "less";
-	else if ( "coffee" == fileType )
-		config.mode = "coffeescript";
-	else if ( "html" == fileType || "htm" == fileType )
-		config.mode = "html";
-	else if ( "php" == fileType ) 
-		config.mode = "php";
-	else 
-		config.mode = "markdown";
+	switch ( fileType ) {
+		case "js":
+			config.mode = "javascript";
+		break;
+		case "css":
+			config.mode = "css";
+		break;
+		case "less":
+			config.mode = "less";
+		break;
+		case "coffee":
+			config.mode = "coffeescript";
+		break;
+		case "html":
+		case "htm":
+			config.mode = "html";
+		break;
+		case "php":
+			config.mode = "php";
+		break;
+		default:
+			config.mode = "markdown";
+		break;
+	}
 	
 	CodeMirror.commands.save = function (){ jQuery('#submit').click(); };
 	
-	CodeMirror.fromTextArea( $( '#newcontent' ).get(0), config );
+	CodeMirror.fromTextArea( $new.get(0), config );
 	
 	if ( height < $( '.CodeMirror-scroll ' ).height() ) $( '.CodeMirror-scroll ' ).height( height );
 });
