df877c9b6fdbcc87a40f9a01920855ab32a72c74
Ken Newman <wraithkenny@unfocus.com>
2013-06-25 21:09:35 -0400

    cm docs suggest Infinity

--- js/settings-page.js
+++ js/settings-page.js
@@ -4,6 +4,7 @@ jQuery( document ).ready( function( $ ) {
 	var theme = codemirror_options.theme ? codemirror_options.theme: 'default';
 	var editor = CodeMirror.fromTextArea(document.getElementById("codemirror_demo"), {
 		lineNumbers: true,
+		viewportMargin: Infinity,
 		matchBrackets: true,
 		mode: "application/x-httpd-php",
 		indentUnit: 4,
