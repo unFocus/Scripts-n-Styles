de21507c0d0a2ad6f83cc8cc972a5d4973a711b5
Ken Newman <wraithkenny@unfocus.com>
2016-12-12 11:37:52 -0500

    Add new CodeMirror Themes

--- scripts-n-styles.php
+++ scripts-n-styles.php
@@ -70,13 +70,24 @@ class Scripts_n_Styles
 	 */
 	const VERSION = '4.0.0-alpha';
 	static $file = __FILE__;
-	static $cm_themes = array( 'default', '3024-day', '3024-night', 'ambiance',
-		'base16-dark', 'base16-light',
-		'blackboard', 'cobalt', 'eclipse',
-		'elegant', 'erlang-dark', 'lesser-dark', 'midnight', 'monokai',
-		'neat', 'night', 'paraiso-dark', 'paraiso-light', 'rubyblue',
-		'solarized', 'the-matrix', 'tomorrow-night-eighties', 'twilight', 'vibrant-ink',
-		'xq-dark', 'xq-light' );
+	static $cm_themes = array( 'default',
+		'3024-day', '3024-night', 'abcdef', 'ambiance',
+		'base16-dark', 'base16-light', 'bespin', 'blackboard',
+		'cobalt', 'colorforth',
+		'dracula', 'duotone-dark', 'duotone-light',
+		'eclipse', 'elegant', 'erlang-dark',
+		'hopscotch', 'icecoder', 'isotope',
+		'lesser-dark', 'liquibyte',
+		'material', 'mbo', 'mdn-like', 'midnight', 'monokai',
+		'neat', 'neo', 'night',
+		'panda-syntax', 'paraiso-dark', 'paraiso-light', 'pastel-on-dark',
+		'railscasts', 'rubyblue',
+		'seti', 'solarized',
+		'the-matrix', 'tomorrow-night-bright', 'tomorrow-night-eighties',
+		'ttcn', 'twilight',
+		'vibrant-ink',
+		'xq-dark', 'xq-light',
+		'yeti', 'zenburn' );
 	/**#@-*/
 
 	/**
