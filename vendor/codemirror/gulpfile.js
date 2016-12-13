fecf2c277a389c75536212dd5e96f79537fa0716
Ken Newman <wraithkenny@unfocus.com>
2016-12-12 22:34:09 -0500

    build a full CodeMirror, concat, and min, and use that instead.

--- vendor/codemirror/gulpfile.js
+++ vendor/codemirror/gulpfile.js
@@ -3,25 +3,39 @@ var gulp = require('gulp');
 // Include plugins
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+
+var dir = 'node_modules/codemirror';
+gulp.task('css', function(){
+    return gulp.src([
+        dir+'/lib/**/*.css',
+        dir+'/theme/**/*.css',
+        dir+'/mode/**/*.css',
+        dir+'/addon/**/*.css'
+    ])
+    .pipe(concat('codemirror.min.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+        'diff_match_patch.js',
+        dir+'/lib/**/*.js',
+        dir+'/theme/**/*.js',
+        dir+'/keymap/**/*.js',
+
+        dir+'/addon/**/*.js',
+        '!'+dir+'/addon/runmode/**/*.js',
+        '!'+dir+'/addon/tern/**/*.js',
+        '!'+dir+'/addon/mode/multiplex_test.js',
+        '!'+dir+'/addon/mode/loadmode.js',
+        dir+'/mode/**/*.js',
+    ])
+    .pipe(concat('codemirror.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('.'));
+});
 
 // Default Task
-gulp.task('default', function() {
-    return gulp.src([
-        'node_modules/codemirror/lib/codemirror.js',
-        'node_modules/codemirror/mode/css/css.js',
-        'node_modules/codemirror/mode/coffeescript/coffeescript.js',
-        'node_modules/codemirror/mode/less/less.js',
-        'node_modules/codemirror/mode/javascript/javascript.js',
-        'node_modules/codemirror/mode/xml/xml.js',
-        'node_modules/codemirror/mode/clike/clike.js',
-        'node_modules/codemirror/mode/markdown/markdown.js',
-        'node_modules/codemirror/mode/gfm/gfm.js',
-        'node_modules/codemirror/mode/htmlmixed/htmlmixed.js',
-        'node_modules/codemirror/mode/php/php.js',
-    ])
-    .pipe(concat('codemirror.js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('./'));
-});
+gulp.task('default', ['css','js']);
