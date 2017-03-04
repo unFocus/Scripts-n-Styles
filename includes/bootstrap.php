c842863ac438630c9228dbf506267bae77ff2971
Ken Newman <wraithkenny@unfocus.com>
2017-03-04 02:52:09 -0500

    move stub to main

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,18 +1,6 @@
 <?php
 namespace unFocus\SnS;
 
-
-add_action( 'wp_before_admin_bar_render', function() {
-	global $wp_admin_bar;
-	$wp_admin_bar->add_node( [
-		'id'    => 'Scripts_n_Styles',
-		'title' => 'Scripts n Styles',
-		'href'  => '#',
-		'meta'  => array( 'class' => 'Scripts_n_Styles' )
-	] );
-}, 11 );
-
 require_once( "constants.php" );
 require_once( "main.php" );
-
 require_once( "class-sns-widget.php" );
--- includes/main.php
+++ includes/main.php
@@ -1,6 +1,16 @@
 <?php
 namespace unFocus\SnS;
 
+add_action( 'wp_before_admin_bar_render', function() {
+	global $wp_admin_bar;
+	$wp_admin_bar->add_node( [
+		'id'    => 'Scripts_n_Styles',
+		'title' => 'Scripts n Styles',
+		'href'  => '#',
+		'meta'  => array( 'class' => 'Scripts_n_Styles' )
+	] );
+}, 11 );
+
 register_theme_directory( dirname( __DIR__ ) . '/theme' );
 
 if ( is_admin() && ! ( defined('DISALLOW_UNFILTERED_HTML') && DISALLOW_UNFILTERED_HTML ) ) {
