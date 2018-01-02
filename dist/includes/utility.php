1fcb88cf5fe66bf5e6493eec60332f01973296a4
Ken Newman <wraithkenny@unfocus.com>
2018-01-02 16:53:49 -0500

    No one is still on the version 2 branch.

--- dist/includes/utility.php
+++ dist/includes/utility.php
@@ -2,7 +2,7 @@
 /**
  * Utility functions
  *
- * Mostly just handles upgrade procedures.
+ * Mostly just handles upgrade procedures, but also registers the theme.
  *
  * @package Scripts-N-Styles
  */
@@ -31,60 +31,6 @@ function upgrade() {
 	}
 
 	$version = $options['version'];
-
-	/*
-	 * upgrade proceedure for 3.0 update
-	 */
-	if ( version_compare( $version, '3', '<' ) ) :
-		$posts = get_posts(
-			array(
-				'numberposts' => -1,
-				'post_type' => 'any',
-				'post_status' => 'any',
-				'meta_query' => array(
-					'relation' => 'OR',
-					array( 'key' => '_SnS_scripts' ),
-					array( 'key' => '_SnS_styles' ),
-					array( 'key' => 'uFp_scripts' ),
-					array( 'key' => 'uFp_styles' ),
-				),
-			)
-		);
-
-		if ( $posts ) :
-			foreach ( $posts as $post ) {
-				$styles = get_post_meta( $post->ID, '_SnS_styles', true );
-				if ( empty( $styles ) ) {
-					$styles = get_post_meta( $post->ID, 'uFp_styles', true );
-				}
-
-				$scripts = get_post_meta( $post->ID, '_SnS_scripts', true );
-				if ( empty( $scripts ) ) {
-					$scripts = get_post_meta( $post->ID, 'uFp_scripts', true );
-				}
-
-				$sns = array();
-				if ( ! empty( $styles ) ) {
-					$sns['styles'] = $styles;
-				}
-
-				if ( ! empty( $scripts ) ) {
-					$sns['scripts'] = $scripts;
-				}
-
-				if ( ! empty( $sns ) ) {
-					update_post_meta( $post->ID, '_SnS', $sns );
-				}
-
-				delete_post_meta( $post->ID, 'uFp_styles' );
-				delete_post_meta( $post->ID, 'uFp_scripts' );
-				delete_post_meta( $post->ID, '_SnS_styles' );
-				delete_post_meta( $post->ID, '_SnS_scripts' );
-			}
-		endif; // if $posts .
-
-		$version = '3.0';
-	endif; // 3.0 upgrade
 
 	/*
 	 * upgrade proceedure for 4.0 update
