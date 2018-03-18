9c15814f8e0371f78c6e614f9f01a4f9eae6a86a
Ken Newman <wraithkenny@unfocus.com>
2018-03-17 23:42:43 -0400

    coding styles

--- dist/includes/bootstrap.php
+++ dist/includes/bootstrap.php
@@ -9,14 +9,14 @@
 
 namespace unFocus\SnS;
 
-const BASENAME = 'scripts-n-styles/scripts-n-styles.php';
-const VERSION = '4.0.0-alpha-2';
-const OPTION_GROUP = 'scripts_n_styles';
+const BASENAME        = 'scripts-n-styles/scripts-n-styles.php';
+const VERSION         = '4.0.0-alpha-2';
+const OPTION_GROUP    = 'scripts_n_styles';
 const ADMIN_MENU_SLUG = 'sns';
-const NONCE_NAME = 'scripts_n_styles_noncename';
+const NONCE_NAME      = 'scripts_n_styles_noncename';
 
-require_once( __DIR__ . '/main.php' );
-require_once( __DIR__ . '/utility.php' );
+require_once __DIR__ . '/main.php';
+require_once __DIR__ . '/utility.php';
 
 
 if ( is_admin() && ! ( defined( 'DISALLOW_UNFILTERED_HTML' ) && DISALLOW_UNFILTERED_HTML ) ) {
@@ -26,13 +26,13 @@ if ( is_admin() && ! ( defined( 'DISALLOW_UNFILTERED_HTML' ) && DISALLOW_UNFILTE
 		plugin's admin because no user would have the capability.
 	*/
 
-	require_once( __DIR__ . '/admin.php' );
-	require_once( __DIR__ . '/meta-box.php' );
-	require_once( __DIR__ . '/settings-page.php' );
-	require_once( __DIR__ . '/usage-page.php' );
-	require_once( __DIR__ . '/global-page.php' );
-	require_once( __DIR__ . '/hoops-page.php' );
-	require_once( __DIR__ . '/theme-page.php' );
-	require_once( __DIR__ . '/ajax.php' );
-	require_once( __DIR__ . '/form.php' );
+	require_once __DIR__ . '/admin.php';
+	require_once __DIR__ . '/meta-box.php';
+	require_once __DIR__ . '/settings-page.php';
+	require_once __DIR__ . '/usage-page.php';
+	require_once __DIR__ . '/global-page.php';
+	require_once __DIR__ . '/hoops-page.php';
+	require_once __DIR__ . '/theme-page.php';
+	require_once __DIR__ . '/ajax.php';
+	require_once __DIR__ . '/form.php';
 }
